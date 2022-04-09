35bd40c28b160befa842b233e6c613d532c8daa1
Kay Putra <kaspr.works@gmail.com>
2022-04-09 22:58:02 +1000

    displaying updated map marker

--- script.js
+++ script.js
@@ -13,24 +13,39 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 if(navigator.geolocation){
   navigator.geolocation.getCurrentPosition(function(position){
-    console.log(position.coords)
     const {latitude} = position.coords
-    console.log(latitude)
     const {longitude} = position.coords
-    console.log(longitude)
     console.log(`https://www.google.pt/maps/@${latitude},${longitude}`)
 
     const coords = [latitude, longitude]
 
     const map = L.map('map').setView(coords, 13);
 
+
     L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    L.marker(coords).addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-        .openPopup();
+    map.on('click', function(mapEvent){
+      console.log(mapEvent)
+      const {lat, lng} = mapEvent.latlng;
+      console.log(lat)
+      console.log(lng)
+
+      L.marker([lat, lng])
+      .addTo(map)
+      .bindPopup(L.popup({
+        maxWidth:250, 
+        minWidth:150, 
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup'
+        })
+      )
+      .setPopupContent('Workout')
+      .openPopup();
+    });
+   
   }, function(){
     alert('unable to find location')
   })
